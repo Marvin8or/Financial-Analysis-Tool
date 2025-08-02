dba14d5a5533aa9e4b97f7f1f503b3b5f0e7c18a
Marvin8or <gabrielmarvin18@gmail.com>
2025-08-02 17:40:33 +0200

    Added argument parsing

--- get_data.py
+++ get_data.py
@@ -4,10 +4,35 @@ Created on Sat Aug  2 16:59:43 2025
 
 @author: gabri
 """
+import os
+import argparse
 import yfinance as yf
+from datetime import datetime
 
-# Download historical data for Apple (AAPL)
-data = yf.download("AAPL", start="2020-01-01", end="2023-01-01")
+data = yf.download("I500.DE", period="max")
 
-# Display the first 5 rows of the data
 print(data.head())
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--ticker", required=True, type=str, help="Stock or ETF ticker"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    data = yf.download(args.ticker, period="max")
+
+    if not data.empty:
+        print(f"Successfully downloaded data for {args.ticker}.")
+        print(data.head())
+
+        filename = f"{args.ticker}_{timestamp}.csv"
+        data.to_csv(os.path.join("datasets", filename))
+
+        print(f"\nData saved to '{filename}' in folder 'datasets'.")
