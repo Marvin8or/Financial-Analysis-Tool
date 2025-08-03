bb961d918207bf481895617c9481972d78ac5aa8
Marvin8or <gabrielmarvin18@gmail.com>
2025-08-03 21:45:03 +0200

    Added constructor to 'VisualisationEngine'

--- visualisation/visualisation_engine.py
+++ visualisation/visualisation_engine.py
@@ -5,7 +5,11 @@ Created on Sun Aug  3 21:33:39 2025
 @author: gabri
 """
 import matplotlib.pyplot as plt
+from typing import Dict
+
+ResultsDict = Dict
 
 
 class VisualisationEngine:
-    pass
+    def __init__(self, data: ResultsDict):
+        self.results_dict = data
