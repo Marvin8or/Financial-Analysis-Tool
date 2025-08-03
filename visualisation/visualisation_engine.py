3994b7214b35a4dc9e8b341203d63509977482e4
Marvin8or <gabrielmarvin18@gmail.com>
2025-08-03 21:51:19 +0200

    added arguments in '__init__'

--- visualisation/visualisation_engine.py
+++ visualisation/visualisation_engine.py
@@ -4,6 +4,7 @@ Created on Sun Aug  3 21:33:39 2025
 
 @author: gabri
 """
+import os
 import matplotlib.pyplot as plt
 from typing import Dict
 
@@ -11,5 +12,12 @@ ResultsDict = Dict
 
 
 class VisualisationEngine:
-    def __init__(self, data: ResultsDict):
+    def __init__(self, raw_data: pd.DataFrame, data: ResultsDict, results_path):
+        self.raw_data = raw_data
         self.results_dict = data
+        self.results_path = results_path
+        
+        os.makedirs(self.results_path, exist_ok=True)
+
+    def visualise_daily_percentage_change(self):
+        
